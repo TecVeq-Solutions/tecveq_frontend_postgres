ff9581f7dc27194ff3843a555625e729a1575724
HamzaChaudhary786 <hmzsattar99@gmail.com>
2026-04-05 14:35:48 +0500

    firebase default image

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://lms.tecveq.com/api"
 export const BACKEND_URL_SOCKET = import.meta.env.VITE_BACKEND_URL_SOCKET || "https://lms.tecveq.com"
-export const default_profile = "https://firebasestorage.googleapis.com/v0/b/tca-backend-454ca.appspot.com/o/file-path?alt=media&token=88e392aa-8182-4f24-bbc1-e9f3e58082f0"
+export const default_profile = ""
