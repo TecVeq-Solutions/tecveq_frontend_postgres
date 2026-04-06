ce6ea8e33735441e5c69ce16e40112554ff4bee6
HamzaChaudhary786 <hmzsattar99@gmail.com>
2026-04-06 18:27:34 +0500

    api change

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://lms.tecveq.com/api"
-export const BACKEND_URL_SOCKET = import.meta.env.VITE_BACKEND_URL_SOCKET || "https://lms.tecveq.com"
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://api.tecveq.com/api"
+export const BACKEND_URL_SOCKET = import.meta.env.VITE_BACKEND_URL_SOCKET || "https://api.tecveq.com"
 export const default_profile = ""
